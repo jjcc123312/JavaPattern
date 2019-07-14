4ea846855357fe48373d06e69fb5cce2475f7487
jjcc123312 <1542123803@qq.com>
2019-07-14 14:39:28 +0800

    1

---
+++ src/cn/demo/factorys/review/Client2.java
@@ -0,0 +1,11 @@
+package cn.demo.factorys.review;
+
+/**
+ * @author Jjcc
+ * @version 1.0.0
+ * @Description
+ * @ClassName Client2.java
+ * @createTime 2019年07月14日 14:28:00
+ */
+public class Client2 {
+}
